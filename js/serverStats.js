f5e357d74e247e4a6062824846fda2b64983aa87
Samuli Ruuskanen <samuli.ruuskanen@tuni.fi>
2022-01-18 15:31:37 +0200

    Added player list

---
+++ js/serverStats.js
@@ -0,0 +1,16 @@
+getJSON("https://api.minetools.eu/query/play.ecocraft.fi/25565").then(response=>{
+    try{
+        console.log(response.response);
+        document.querySelector("#playerCount").textContent = response.response.Players;
+        if(response.response.Players > 0){
+            response.response.Playerlist.forEach(user => {
+                const newUser = document.createElement('p')
+                newUser.textContent = textContent = user;
+                document.querySelector('#playerList').append(newUser);
+            });
+        }
+    }
+    catch (e){
+        document.querySelector("#playerCount").textContent = '-';
+    }
+})
