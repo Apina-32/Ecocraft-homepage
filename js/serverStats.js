bd3ea059ca483f1a98e013fdeb0826213c9a56ec
Samuli Ruuskanen <samuli.ruuskanen@tuni.fi>
2022-01-18 16:03:47 +0200

    Added eventlistener to serverStatus

--- js/serverStats.js
+++ js/serverStats.js
@@ -1,16 +1,33 @@
-getJSON("https://api.minetools.eu/query/play.ecocraft.fi/25565").then(response=>{
-    try{
-        console.log(response.response);
-        document.querySelector("#playerCount").textContent = response.response.Players;
-        if(response.response.Players > 0){
-            response.response.Playerlist.forEach(user => {
-                const newUser = document.createElement('p')
-                newUser.textContent = user;
-                document.querySelector('#playerList').append(newUser);
-            });
+const serverStatus = document.querySelector('.serverStatus');
+
+const updateAddServerStatus = () => {
+    // Timeout event listener to reduce the amount of api calls.
+    serverStatus.removeEventListener('mouseenter', updateAddServerStatus);
+    window.setTimeout(()=>{
+        serverStatus.addEventListener('mouseenter', updateAddServerStatus);
+    }, 10000);
+
+    getJSON("https://api.minetools.eu/query/play.ecocraft.fi/25565").then(response=>{
+        try{
+            console.log(response.response);
+            document.querySelector("#playerCount").textContent = response.response.Players;
+            const playerList = document.querySelector('#playerList');
+            removeChildren(playerList);
+            if(response.response.Players > 0){
+                response.response.Playerlist.forEach(user => {
+                    const newUser = document.createElement('p')
+                    newUser.textContent = user;
+                    playerList.append(newUser);
+                });
+            }
+            serverStatus.setAttribute("last-updated", window.tim)
         }
-    }
-    catch (e){
-        document.querySelector("#playerCount").textContent = '-';
-    }
-})
+        catch (e){
+            document.querySelector("#playerCount").textContent = '-';
+        }
+    })
+}
+
+serverStatus.addEventListener('mouseenter', updateAddServerStatus);
+
+updateAddServerStatus();
