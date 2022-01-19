ab066693f5007cb851c2cfcbccd4455ae1b75f3d
GamerXbot <GamerXbot@hotmail.com>
2022-01-19 18:11:54 +0200

    commands.js

--- js/commands.js
+++ js/commands.js
@@ -2,7 +2,7 @@ if ('content' in document.createElement('template')) {
     const template = document.querySelector("#card-template");
     const container = document.querySelector('#card-container');
 
-    getJSON("/data/news.json").then(news => {
+    getJSON("/data/commands.json").then(news => {
         Object.keys(news.response).forEach(item => {
             const clone = template.content.cloneNode(true);
             clone.querySelector('.title').textContent = item;
