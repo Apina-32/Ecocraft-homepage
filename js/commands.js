b4fd51626cd9561222f857dfd80e38edb62b3d49
Samuli Ruuskanen <samuli.ruuskanen@tuni.fi>
2022-01-19 18:46:28 +0200

    Fixed json reading and output

--- js/commands.js
+++ js/commands.js
@@ -2,13 +2,18 @@ if ('content' in document.createElement('template')) {
     const template = document.querySelector("#card-template");
     const container = document.querySelector('#card-container');
 
-    getJSON("/data/commands.json").then(news => {
-        Object.keys(news.response).forEach(item => {
+    getJSON("/data/commands.json").then(titles => {
+        Object.keys(titles.response).forEach(item => {
             const clone = template.content.cloneNode(true);
             clone.querySelector('.title').textContent = item;
-            const newsContent = news.response[item];
-            clone.querySelector(".text-content").textContent = newsContent.text;
-            clone.querySelector(".date").textContent = newsContent.date;
+            const commands = titles.response[item];
+            Object.keys(commands).forEach(command => {
+                const listElem = document.createElement('li');
+                listElem.textContent = command;
+                listElem.appendChild(document.createElement('p'));
+                listElem.children[0].textContent = commands[command];
+                clone.querySelector(".commands").appendChild(listElem);
+            });
             container.appendChild(clone);
         });
     });
